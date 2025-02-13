8cb85e0811d2f6f2eb7f4cbb887704b9b3a9d8e5
Jonathan Perkin <jperkin@smartos.org>
2025-02-13 11:32:07 +0000

    libuuid: Fix build on systems that lack ECHOPRT.
    While here clean up lots of pkglint.

--- devel/libuuid/patches/patch-include_ttyutils.h
+++ devel/libuuid/patches/patch-include_ttyutils.h
@@ -2,9 +2,9 @@ $NetBSD: patch-include_ttyutils.h,v 1.1 2017/12/08 08:03:35 adam Exp $
 
 Define values if missing.
 
---- include/ttyutils.h.orig	2016-11-02 12:57:31.535167960 +0000
+--- include/ttyutils.h.orig	2018-07-11 13:34:54.227003733 +0000
 +++ include/ttyutils.h
-@@ -60,6 +60,18 @@ extern int get_terminal_name(const char
+@@ -61,6 +61,18 @@ extern int get_terminal_name(const char
  static inline void reset_virtual_console(struct termios *tp, int flags)
  {
  	/* Use defaults of <sys/ttydefaults.h> for base settings */
@@ -23,10 +23,13 @@ Define values if missing.
  	tp->c_iflag |= TTYDEF_IFLAG;
  	tp->c_oflag |= TTYDEF_OFLAG;
  	tp->c_lflag |= TTYDEF_LFLAG;
-@@ -114,6 +126,13 @@ static inline void reset_virtual_console
+@@ -115,6 +127,16 @@ static inline void reset_virtual_console
  # define FFDLY 0
  #endif
  
++#ifndef ECHOPRT
++#define ECHOPRT 0
++#endif
 +#ifndef TAB0
 +#define TAB0 0
 +#endif
@@ -37,7 +40,7 @@ Define values if missing.
  	tp->c_iflag |=  (BRKINT | ICRNL | IMAXBEL);
  	tp->c_iflag &= ~(IGNBRK | INLCR | IGNCR | IXOFF | IUCLC | IXANY | ISTRIP);
  	tp->c_oflag |=  (OPOST | ONLCR | NL0 | CR0 | TAB0 | BS0 | VT0 | FF0);
-@@ -158,7 +177,13 @@ static inline void reset_virtual_console
+@@ -159,7 +181,13 @@ static inline void reset_virtual_console
  	tp->c_cc[VSTOP]    = CSTOP;
  	tp->c_cc[VSUSP]    = CSUSP;
  	tp->c_cc[VEOL]     = _POSIX_VDISABLE;
