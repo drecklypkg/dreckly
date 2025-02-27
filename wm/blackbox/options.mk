4cc9c3d8d1f3c4d559479f1856dec22ccd3a9a25
nia <nia@NetBSD.org>
2025-02-27 17:23:53 +0100

    blackbox: Remove nls option
    Related to issue #45

--- wm/blackbox/options.mk
+++ wm/blackbox/options.mk
@@ -1,8 +1,8 @@
 # $NetBSD: options.mk,v 1.2 2021/06/11 20:14:37 gutteridge Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.blackbox
-PKG_SUPPORTED_OPTIONS=	nls xft2
-PKG_SUGGESTED_OPTIONS=	nls xft2
+PKG_SUPPORTED_OPTIONS=	xft2
+PKG_SUGGESTED_OPTIONS=	xft2
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -14,19 +14,6 @@ PKG_OPTIONS_DEPRECATED_WARNINGS+=	\
 .endif
 
 .include "../../mk/bsd.options.mk"
-
-PLIST_VARS+=	nls
-
-# The nls option also controls whether the fr/ja/nl/sl man pages are
-# installed.
-.if !empty(PKG_OPTIONS:Mnls)
-BROKEN_GETTEXT_DETECTION=	yes
-CONFIGURE_ARGS+=	--enable-nls
-.include "../../devel/gettext-lib/buildlink3.mk"
-PLIST.nls=	yes
-.else
-CONFIGURE_ARGS+=	--disable-nls
-.endif
 
 .if !empty(PKG_OPTIONS:Mxft2)
 CONFIGURE_ARGS+=	--enable-xft
