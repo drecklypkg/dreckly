497844f08bb7e876ff55350880883249fe307868
Jonathan Perkin <jperkin@smartos.org>
2025-03-11 11:31:10 +0000

    libtorrent: Update to 0.15.1.

--- net/libtorrent/buildlink3.mk
+++ net/libtorrent/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	libtorrent
 .if !defined(LIBTORRENT_BUILDLINK3_MK)
 LIBTORRENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.14.0
-BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.14.0
+BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.15.1
+BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.15.1
 BUILDLINK_PKGSRCDIR.libtorrent?=	../../net/libtorrent
 
 .include "../../security/openssl/buildlink3.mk"
--- net/libtorrent/patches/patch-src_data_memory__chunk.cc
+++ net/libtorrent/patches/patch-src_data_memory__chunk.cc
@@ -1,15 +1,21 @@
 $NetBSD: patch-src_data_memory__chunk.cc,v 1.1 2023/11/06 11:26:45 nia Exp $
 
-Fix Sun-specific definition of madvise.
+Remove bogus madvise() forward prototype.
 
---- src/data/memory_chunk.cc.orig	2019-07-19 11:44:08.000000000 +0000
+--- src/data/memory_chunk.cc.orig	2025-01-01 12:58:24.000000000 +0000
 +++ src/data/memory_chunk.cc
-@@ -45,7 +45,7 @@
+@@ -44,14 +44,6 @@
+ #include "torrent/exceptions.h"
  #include "memory_chunk.h"
  
- #ifdef __sun__
+-#ifdef __sun__
 -extern "C" int madvise(void *, size_t, int);
-+extern "C" int madvise(caddr_t, size_t, int);
- //#include <sys/mman.h>
- //Should be the include line instead, but Solaris
- //has an annoying bug wherein it doesn't declare
+-//#include <sys/mman.h>
+-//Should be the include line instead, but Solaris
+-//has an annoying bug wherein it doesn't declare
+-//madvise for C++.
+-#endif
+-
+ namespace torrent {
+ 
+ uint32_t MemoryChunk::m_pagesize = getpagesize();
