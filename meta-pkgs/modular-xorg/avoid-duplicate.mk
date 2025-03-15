101148657b8eb2b89ec0019ee806f551b0d5be06
nia <nia@NetBSD.org>
2025-03-15 13:16:29 +0100

    ocaml-mirage-flow: Remove.

--- meta-pkgs/modular-xorg/avoid-duplicate.mk
+++ meta-pkgs/modular-xorg/avoid-duplicate.mk
@@ -5,15 +5,15 @@
 .if ${X11_TYPE} == "native"
 .  if !empty(X11_BIN_NAME)
 .    if exists(${X11BASE}/bin/${X11_BIN_NAME})
-#PKG_SKIP_REASON+=	"Package set is using native X11 component"
+PKG_SKIP_REASON+=	"Package set is using native X11 component"
 .    endif
 .  elif !empty(X11_LIB_NAME)
 .    if exists(${X11BASE}/lib/${X11_LIB_NAME}.so)
-#PKG_SKIP_REASON+=	"Package set is using native X11 component"
+PKG_SKIP_REASON+=	"Package set is using native X11 component"
 .    endif
 .  elif !empty(X11_DRV_NAME)
 .    if exists(${X11BASE}/lib/modules/drivers/${X11_DRV_NAME}_drv.so)
-#PKG_SKIP_REASON+=	"Package set is using native X11 component"
+PKG_SKIP_REASON+=	"Package set is using native X11 component"
 .    endif
 .  endif
 .endif
