17116fb30ebf98a888a0b8b9cf5fb3ab438a5849
Jonathan Perkin <jperkin@smartos.org>
2025-03-19 15:10:02 +0000

    gcc14-libs: Test overhaul.

--- lang/gcc14-libs/buildlink3.mk
+++ lang/gcc14-libs/buildlink3.mk
@@ -5,31 +5,45 @@ BUILDLINK_TREE+=	gcc14-libs
 .if !defined(GCC14_LIBS_BUILDLINK3_MK)
 GCC14_LIBS_BUILDLINK3_MK:=
 
-.if !empty(USE_PKGSRC_GCC_RUNTIME:M[Yy][Ee][Ss])
-BUILDLINK_API_DEPENDS.gcc14-libs+=	gcc14-libs>=14.1.0
-.else
-BUILDLINK_API_DEPENDS.gcc14-libs+=	{gcc14,gcc14-libs}>=14.1.0
-.endif
+BUILDLINK_API_DEPENDS.gcc14-libs+=	gcc14-libs>=0
 BUILDLINK_PKGSRCDIR.gcc14-libs=		../../lang/gcc14-libs
 BUILDLINK_DEPMETHOD.gcc14-libs?=	full
 
 BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_PREFIX.gcc14-libs}/gcc14
-
-BUILDLINK_FILES.gcc14-libs=	#empty
+BUILDLINK_FILES.gcc14-libs=	# empty
 BUILDLINK_AUTO_VARS.gcc14-libs=	no
 
-.if !empty(USE_PKGSRC_GCC_RUNTIME:M[Yy][Ee][Ss])
-# Use custom specs file to ensure we link against pkgsrc libraries.
+# Use custom specs file to ensure we link against the GCC libraries from
+# this package.  Some OPSYS also have additional handling so support custom
+# source files.
+#
+OPSYSVARS+=		SPECS_SRC
+SPECS_SRC.SunOS=	${BUILDLINK_PKGSRCDIR.gcc14-libs}/files/specs.illumos
+SPECS_SRC.*=		${BUILDLINK_PKGSRCDIR.gcc14-libs}/files/specs
+
 SPECS_LIBGCC=		${WRAPPER_DIR}/specs.libgcc
 WRAPPER_TARGETS+=	${SPECS_LIBGCC}
 
-LIBGCC_SUBPREFIX=	gcc14
-LIBGCC_PREFIX=		${BUILDLINK_PREFIX.gcc14-libs}/${LIBGCC_SUBPREFIX}
-GCC_TARGET_MACHINE?=	${MACHINE_GNU_PLATFORM}
+LIBGCC_PREFIX=	${BUILDLINK_PREFIX.gcc14-libs}/gcc14/${MACHINE_GNU_PLATFORM}
+
+SPECS_SED=	-e '/^\#/d'
+SPECS_SED+=	-e 's,@LINKER_RPATH_FLAG@,${LINKER_RPATH_FLAG},g'
+SPECS_SED+=	-e 's,@LIBGCC_PREFIX@,${LIBGCC_PREFIX},g'
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+# The specs file for illumos additionally has some sanitisation of the default
+# library search path.
+.if ${MACHINE_PLATFORM:MSunOS-*-x86_64}
+SPECS_SED+=	-e 's,@LINK_ARCH_DEFAULT@,link_arch64,g'
+SPECS_SED+=	-e 's,@LINK_LIBGCC_DEFAULT@,link_libgcc_arch64,g'
+.elif ${MACHINE_PLATFORM:MSunOS-*-i386}
+SPECS_SED+=	-e 's,@LINK_ARCH_DEFAULT@,link_arch32,g'
+SPECS_SED+=	-e 's,@LINK_LIBGCC_DEFAULT@,link_libgcc_arch32,g'
+.endif
 
 ${SPECS_LIBGCC}:
-	@${ECHO} "*link_libgcc:" >${SPECS_LIBGCC}
-	@${ECHO} "%D ${LINKER_RPATH_FLAG}${LIBGCC_PREFIX}/${GCC_TARGET_MACHINE}/lib/%M" >>${SPECS_LIBGCC}
+	${SED} ${SPECS_SED} < ${SPECS_SRC} > ${SPECS_LIBGCC}
 
 _WRAP_EXTRA_ARGS.CC+=	-specs=${SPECS_LIBGCC}
 _WRAP_EXTRA_ARGS.CXX+=	-specs=${SPECS_LIBGCC}
@@ -37,10 +51,9 @@ _WRAP_EXTRA_ARGS.FC+=	-specs=${SPECS_LIBGCC}
 CWRAPPERS_APPEND.cc+=	-specs=${SPECS_LIBGCC}
 CWRAPPERS_APPEND.cxx+=	-specs=${SPECS_LIBGCC}
 CWRAPPERS_APPEND.f77+=	-specs=${SPECS_LIBGCC}
-.endif
 
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
-.endif # GCC14_LIBS_BUILDLINK3_MK
+.endif
 
 BUILDLINK_TREE+=	-gcc14-libs
