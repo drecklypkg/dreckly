d425f14e057da438043d9d7d9d49126e661423bd
Jason W. Bacon <bacon4000@gmail.com>
2025-04-26 17:07:35 -0500

    devel/libxtend: Update to 0.2.1
    Several bug fixes and enhancements
    Changes: https://github.com/outpaddling/libxtend/releases

--- devel/libxtend/buildlink3.mk
+++ devel/libxtend/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.15 2023/12/28 15:02:17 bacon Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=		libxtend
 
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.0.2
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.1
 BUILDLINK_PKGSRCDIR.libxtend?=		../../devel/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
