5071537c63abad54b8274097bc9a19357c9e7589
Jason W. Bacon <bacon4000@gmail.com>
2025-04-26 18:28:26 -0500

    biolibc: Update to 0.2.7
    Minor fixes and enhancements
    Changes: https://github.com/auerlab/biolibc/releases

--- biology/biolibc/buildlink3.mk
+++ biology/biolibc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.14 2024/05/19 12:54:59 bacon Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.6.4
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.7
 BUILDLINK_PKGSRCDIR.biolibc?=	../../biology/biolibc
 
 .include "../../devel/libxtend/buildlink3.mk"
