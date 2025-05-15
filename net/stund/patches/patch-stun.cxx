8aee03eeea6abe43fc7cd7f05664fa164fef023a
nia <nia@NetBSD.org>
2025-05-15 11:11:36 +0200

    stund: Improve portability of RNG seeding.

--- net/stund/patches/patch-stun.cxx
+++ net/stund/patches/patch-stun.cxx
@@ -1,5 +1,7 @@
 $NetBSD: patch-stun.cxx,v 1.2 2024/10/10 20:18:02 nia Exp $
 
+Portable RNG seeding.
+
 --- stun.cxx.orig	2012-01-26 11:02:46.000000000 +0000
 +++ stun.cxx
 @@ -16,6 +16,7 @@
@@ -10,10 +12,14 @@ $NetBSD: patch-stun.cxx,v 1.2 2024/10/10 20:18:02 nia Exp $
  #include <sys/time.h>
  #include <sys/types.h> 
  #include <arpa/inet.h>
-@@ -673,12 +674,10 @@ stunRand()
-       asm("rdtsc" : "=A" (tick));
- #elif defined (__SUNPRO_CC) || defined( __sparc__ )	
-       tick = gethrtime();
+@@ -669,16 +670,10 @@ stunRand()
+       tick = hightick;
+       tick <<= 32;
+       tick |= lowtick;
+-#elif defined(__GNUC__) && ( defined(__i686__) || defined(__i386__) )
+-      asm("rdtsc" : "=A" (tick));
+-#elif defined (__SUNPRO_CC) || defined( __sparc__ )	
+-      tick = gethrtime();
 -#elif defined(__MACH__)  || defined(__linux)
 -      int fd=open("/dev/random",O_RDONLY);
 +#else
