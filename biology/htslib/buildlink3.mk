f0740a1b42e1e2ee054efe34dda7ddfae5ce93bc
Jason Bacon <bacon4000@gmail.com>
2025-06-25 07:14:58 -0500

    biology/htslib: Update to 1.22
    Numerous fixes and enhancements
    Changes: https://github.com/samtools/htslib/releases
    Tested all dependent ports, no breaking API/ABI changes

--- biology/htslib/buildlink3.mk
+++ biology/htslib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2024/10/11 22:36:15 bacon Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	htslib
 
 .if !defined(HTSLIB_BUILDLINK3_MK)
 HTSLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.21
-BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.21
+BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.22
+BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.22
 BUILDLINK_PKGSRCDIR.htslib?=	../../biology/htslib
 
 .endif	# HTSLIB_BUILDLINK3_MK
