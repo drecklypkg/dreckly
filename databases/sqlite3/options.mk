da7188b5fdcdd91f9cbf026e5a1e80d51da9fe02
nia <nia@NetBSD.org>
2025-08-01 11:44:41 +0200

    sqlite3: Update features for new build system.

--- databases/sqlite3/options.mk
+++ databases/sqlite3/options.mk
@@ -11,12 +11,15 @@ CFLAGS+=	-DSQLITE_ENABLE_DBSTAT_VTAB=1
 .endif
 
 .if !empty(PKG_OPTIONS:Mfts)
-CFLAGS+=	-DSQLITE_ENABLE_FTS4=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS=1
+CONFIGURE_ARGS+=	--fts3
+CONFIGURE_ARGS+=	--fts4
+CONFIGURE_ARGS+=	--fts5
 .endif
 
 .if !empty(PKG_OPTIONS:Micu)
-CFLAGS+=	-DSQLITE_ENABLE_ICU=1
-LDFLAGS+=	-licui18n -licuuc -licudata
+CONFIGURE_ARGS+=	--icu-collations
+CONFIGURE_ARGS+=	--with-icu-cflags=-I${BUILDLINK_PREFIX.icu}/include
+CONFIGURE_ARGS+=	--with-icu-ldflags="-licui18n -licuuc -licudata"
 .include "../../textproc/icu/buildlink3.mk"
 .endif
 
