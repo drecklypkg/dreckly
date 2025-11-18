53d894b75604863e817004b362d7393fa0f8586e
nia <nia@NetBSD.org>
2025-11-18 13:11:59 +0100

    harfbuzz: Remove "doc" option

--- fonts/harfbuzz/options.mk
+++ fonts/harfbuzz/options.mk
@@ -1,23 +1,13 @@
 # $NetBSD: options.mk,v 1.18 2024/05/20 21:21:38 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.harfbuzz
-PKG_SUPPORTED_OPTIONS+=	doc introspection
-PKG_SUGGESTED_OPTIONS=	doc
+PKG_SUPPORTED_OPTIONS+=	introspection
 
 PKG_SUGGESTED_OPTIONS+=	${${USE_CROSS_COMPILE:tl} == "yes":?:introspection}
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		doc introspection
-
-PRINT_PLIST_AWK+=	{ if (/^share\/gtk-doc/) $$0 = "$${PLIST.doc}" $$0 }
-.if !empty(PKG_OPTIONS:Mdoc)
-MESON_ARGS+=		-Ddocs=enabled
-TOOL_DEPENDS+=		gtk-doc>=1.32nb9:../../textproc/gtk-doc
-PLIST.doc=		yes
-.else
-MESON_ARGS+=		-Ddocs=disabled
-.endif
+PLIST_VARS+=		introspection
 
 PRINT_PLIST_AWK+=	{ if (/^bin\/hb-info/) $$0 = "$${PLIST.introspection}" $$0 }
 PRINT_PLIST_AWK+=	{ if (/^bin\/hb-ot-shape-closure/) $$0 = "$${PLIST.introspection}" $$0 }
