a99a7831c267524c459d9ca1d441e33ed7a48b64
nia <nia@NetBSD.org>
2025-11-18 16:28:11 +0100

    groff: Disable docs support

--- textproc/groff/options.mk
+++ textproc/groff/options.mk
@@ -1,21 +1,10 @@
 # $NetBSD: options.mk,v 1.14 2024/10/14 06:45:59 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.groff
-PKG_SUPPORTED_OPTIONS=	groff-docs x11
-PKG_SUGGESTED_OPTIONS=	groff-docs x11
+PKG_SUPPORTED_OPTIONS=	x11
+PKG_SUGGESTED_OPTIONS=	x11
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mgroff-docs)
-DEPENDS+=		netpbm>=10.0:../../graphics/netpbm
-DEPENDS+=		${PYPKGPREFIX}-pspdfutils-[0-9]*:../../print/py-pspdfutils
-USE_TOOLS+=		gs:run
-PLIST_SRC+=		PLIST.docs
-
-.include "../../lang/python/pyversion.mk"
-.else
-CONFIGURE_ENV+=		with_doc=no
-.endif
 
 .if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--with-x
