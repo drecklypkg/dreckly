91fcb22f579e2a318ba411ddc1f1eaad814d6df5
nia <nia@NetBSD.org>
2026-01-03 13:05:17 +0100

    xine-lib: Update to 1.2.13

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
 BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.12nb19
 BUILDLINK_PKGSRCDIR.xine-lib?=		../../multimedia/xine-lib
 
-.include "../../multimedia/ffmpeg4/buildlink3.mk"
+.include "../../multimedia/ffmpeg6/buildlink3.mk"
 .endif # XINE_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xine-lib
