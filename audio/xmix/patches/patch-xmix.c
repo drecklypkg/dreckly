4d1b8d4e83803583c7e8ff5a84c31d53b77dc565
nia <nia@NetBSD.org>
2026-06-18 21:32:21 +0200

    xmix: Fix build with GCC 14 & 15.
    Avoid some implicit function declarations.

--- audio/xmix/patches/patch-xmix.c
+++ audio/xmix/patches/patch-xmix.c
@@ -1,29 +1,34 @@
 $NetBSD: patch-xmix.c,v 1.1 2019/07/16 10:46:45 micha Exp $
 
-- support more OSes (is the first hunk actually correct?)
+- support more OSes
 - fix void main
 - remove debug printout
 
---- xmix.c.orig	2019-07-03 12:47:36.000000000 +0000
+--- xmix.c.orig	2026-06-18 19:29:24.745505748 +0000
 +++ xmix.c
-@@ -77,7 +77,16 @@ to Rick! I'll call this version V2.1
+@@ -48,8 +48,11 @@ to Rick! I'll call this version V2.1
+ 
+ ======================================================================*/
+ 
++#include <sys/ioctl.h>
++#include <sys/soundcard.h>
+ #include <stdio.h>
+-#include <sys/errno.h>
++#include <stdlib.h>
++#include <errno.h>
+ #include <fcntl.h>
+ 
+ /*
+@@ -77,8 +80,6 @@ to Rick! I'll call this version V2.1
  #include "square_empty.bit"
  #include "square_with_x.bit"
  
-+#if defined(__linux__)
- #include <linux/soundcard.h>
-+#elif defined(__NetBSD__)
-+#include <sys/ioctl.h>
-+#include <soundcard.h>
-+#elif defined(__sun)
-+#include <sys/soundcard.h>
-+#else
-+#include <machine/soundcard.h>
-+#endif
- 
+-#include <linux/soundcard.h>
+-
  #define SOUND_FULL_SCALE 100.0
  #define MAX_SOUND_VOL 95
-@@ -450,7 +459,7 @@ static void set_supported(VolumeControl 
+ #define MIN_SOUND_VOL 05
+@@ -450,7 +451,7 @@ static void set_supported(VolumeControl *vcptr, int is
  }
  
  
@@ -32,7 +37,7 @@ $NetBSD: patch-xmix.c,v 1.1 2019/07/16 10:46:45 micha Exp $
  {
    XtAppContext app_context;
    int scroll_sep, longway;
-@@ -742,7 +751,7 @@ void main (int argc, char **argv)
+@@ -742,7 +743,7 @@ void main (int argc, char **argv)
    mic.locked = 0;
    reclvl.locked = 0;
  
