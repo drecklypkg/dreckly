d514c25eaee2e01b912331a8a627b4cc52621c65
nia <nia@NetBSD.org>
2026-08-06 00:19:06 +0200

    palemoon: Simplify patch.

--- www/palemoon/patches/patch-platform_js_src_wasm_WasmSignalHandlers.cpp
+++ www/palemoon/patches/patch-platform_js_src_wasm_WasmSignalHandlers.cpp
@@ -2,19 +2,16 @@ $NetBSD$
 
 Fix build on newer NetBSD versions.
 
---- platform/js/src/wasm/WasmSignalHandlers.cpp.orig	2026-08-04 14:05:57.629523929 +0000
+PR kern/60539 XSAVE changes break ucontext userspace API
+
+--- platform/js/src/wasm/WasmSignalHandlers.cpp.orig	2026-07-28 13:51:54.000000000 +0000
 +++ platform/js/src/wasm/WasmSignalHandlers.cpp
-@@ -174,7 +174,12 @@ class AutoSetHandlingSegFault
+@@ -174,7 +174,7 @@ class AutoSetHandlingSegFault
  #  define RFP_sig(p) ((p)->uc_mcontext.__gregs[22])
  # endif
  #elif defined(__NetBSD__)
 -# define XMM_sig(p,i) (((struct fxsave64*)(p)->uc_mcontext.__fpregs)->fx_xmm[i])
-+# include <sys/param.h>
-+# if __NetBSD_Prereq__(11,0,0)
-+#  define XMM_sig(p,i) (((struct fxsave64 *)(p)->uc_mcontext.__fpregs.__fxsave)->fx_xmm[i])
-+# else
-+#  define XMM_sig(p,i) (((struct fxsave64*)(p)->uc_mcontext.__fpregs)->fx_xmm[i])
-+# endif
++# define XMM_sig(p,i) (((struct fxsave64*)&(p)->uc_mcontext.__fpregs)->fx_xmm[i])
  # define EIP_sig(p) ((p)->uc_mcontext.__gregs[_REG_EIP])
  # define RIP_sig(p) ((p)->uc_mcontext.__gregs[_REG_RIP])
  # define RAX_sig(p) ((p)->uc_mcontext.__gregs[_REG_RAX])
